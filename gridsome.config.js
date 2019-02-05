4742ad1a116a70ba56be205b72a33247013f899c
Antonio Gil <antonioluisgil@gmail.com>
2019-02-05 19:08:28 +0100

    Add some gridsome configuration data

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,9 @@
-// This is where project configuration and installed plugin options are located. 
+// This is where project configuration and installed plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
+  siteName: 'Antonio Luis Gil',
+  siteDescription: 'Senior Developer',
+  siteUrl: 'antonioluisgil.com',
   plugins: []
-}
+};
